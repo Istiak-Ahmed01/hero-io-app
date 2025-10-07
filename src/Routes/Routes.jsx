0615a3699222cb54b0be4a6c39f6c8a44feaa2db
Istiak-Ahmed01 <istiakefty1@gmail.com>
2025-10-08 01:26:54 +0600

    done some work on navbar

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../pages/Root/Root';
+import Home from '../pages/Home/Home';
+import Allapps from '../pages/Allapps/Allapps';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home
+      },
+      {
+        path: "/allapps",
+        Component:Allapps,
+      }
+    ]
+  },
+]);
